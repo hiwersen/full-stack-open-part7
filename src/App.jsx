d7f134cd861e36931ffc5cb037c9e2065e054253
Hiwersen Angelo Gnocchi Godoy <100746220+hiwersen@users.noreply.github.com>
2025-03-16 20:47:09 +0000

    Implement exercise 7.8

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,20 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then(({ data }) => {
+        setResources(data)
+      })
+  }, [baseUrl])
 
   const create = (resource) => {
-    // ...
+    axios
+      .post(baseUrl, resource)
+      .then(({ data }) => {
+        setResources(resources.concat(data))
+      })
   }
 
   const service = {
